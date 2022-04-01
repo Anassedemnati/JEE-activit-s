b348d15de985c962ba9206fab571b05b32e47801
Anasse Demnati <demnati_anasse@emsi-edu.ma>
2022-04-01 23:48:14 +0200

    Part 1- Spring Boot Spring MVC Thymeleaf

--- patient-mvc/src/main/java/ma/emsi/patientmvc/repositories/PatientRepository.java
+++ patient-mvc/src/main/java/ma/emsi/patientmvc/repositories/PatientRepository.java
@@ -1,7 +1,11 @@
 package ma.emsi.patientmvc.repositories;
 
 import ma.emsi.patientmvc.entites.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient,Long> {
+
+    Page<Patient> findByNomContains(String keyword,Pageable pageable);
 }
--- patient-mvc/src/main/java/ma/emsi/patientmvc/web/PatientController.java
+++ patient-mvc/src/main/java/ma/emsi/patientmvc/web/PatientController.java
@@ -1,12 +1,52 @@
 package ma.emsi.patientmvc.web;
 
 
+import lombok.AllArgsConstructor;
+import ma.emsi.patientmvc.entites.Patient;
 import ma.emsi.patientmvc.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
+
 public class PatientController {
     @Autowired
     PatientRepository patientRepository;
+    @GetMapping(path = "/index")
+    public String patients(Model model,
+                          @RequestParam(name = "page",defaultValue = "0") int page,
+                           @RequestParam(name = "size",defaultValue = "10") int size,
+                           @RequestParam(name = "keyword",defaultValue ="") String keyword) {
+        Page<Patient> pagePatients =patientRepository.findByNomContains(keyword,PageRequest.of(page,size));
+        model.addAttribute("listPatients",pagePatients.getContent());//recupere le contenu de la page en liste
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("keyword",keyword);
+
+        return "patients";
+    }
+    @GetMapping("/delete")
+    public String delete(Long id,String keyword,int page){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/index";
+    }
+    //api rest
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> listPatients(){
+        return patientRepository.findAll();
+    }
+
 }
