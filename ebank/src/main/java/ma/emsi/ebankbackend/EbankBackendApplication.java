21467dc8e67d0771ec68e229d3a4a7ecbe3e0727
Anasse Demnati <demnati_anasse@emsi-edu.ma>
2022-05-18 10:36:23 +0200

    jpa entity inheritance single table

---
+++ ebank/src/main/java/ma/emsi/ebankbackend/EbankBackendApplication.java
@@ -0,0 +1,74 @@
+package ma.emsi.ebankbackend;
+
+import ma.emsi.ebankbackend.entities.AccountOperation;
+import ma.emsi.ebankbackend.entities.CurrentAccount;
+import ma.emsi.ebankbackend.entities.Customer;
+import ma.emsi.ebankbackend.entities.SavingAccount;
+import ma.emsi.ebankbackend.enumes.AccountStatus;
+import ma.emsi.ebankbackend.enumes.OperationType;
+import ma.emsi.ebankbackend.repositories.AccountOperationRepository;
+import ma.emsi.ebankbackend.repositories.BankAccountRepository;
+import ma.emsi.ebankbackend.repositories.CustomerRepository;
+import org.omg.CORBA.Current;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class EbankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankBackendApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner  start(CustomerRepository customerRepository,
+                             BankAccountRepository bankAccountRepository,
+                             AccountOperationRepository accountOperationRepository) {
+        return args -> {
+            Stream.of("Hassan", "Yassine", "Aicha").forEach(name -> {
+                Customer customer = new Customer();
+                customer.setName(name);
+                customer.setEmail(name + "@gmail.com");
+                customerRepository.save(customer);
+            });
+            customerRepository.findAll().forEach(cust -> {
+                CurrentAccount currentAccount = new CurrentAccount();
+                currentAccount.setId(UUID.randomUUID().toString());
+                currentAccount.setBalance(Math.random() * 90000);
+                currentAccount.setCreatedAt(new Date());
+                currentAccount.setStatus(AccountStatus.CREATED);
+                currentAccount.setCustomer(cust);
+                currentAccount.setOverDraft(9000);
+                bankAccountRepository.save(currentAccount);
+
+
+                SavingAccount savingAccount = new SavingAccount();
+                savingAccount.setId(UUID.randomUUID().toString());
+                savingAccount.setBalance(Math.random() * 90000);
+                savingAccount.setCreatedAt(new Date());
+                savingAccount.setStatus(AccountStatus.CREATED);
+                savingAccount.setCustomer(cust);
+                savingAccount.setInterestRate(5.5);
+                bankAccountRepository.save(savingAccount);
+
+
+            });
+            bankAccountRepository.findAll().forEach(acc->{
+                for (int i = 0; i < 5; i++) {
+                    AccountOperation accountOperation = new AccountOperation();
+                    accountOperation.setOperationDate(new Date());
+                    accountOperation.setAmount(Math.random()*12000);
+                    accountOperation.setType(Math.random()>0.5? OperationType.DEBIT:OperationType.CREDIT);
+                    accountOperation.setBankAccount(acc);
+                    accountOperationRepository.save(accountOperation);
+
+                }
+            });
+        };
+    }
+}
